32895e1fc018ff49075b2a01e0ebee2679f3c2fe
TRAVELLINE\denis.bosyi <denis.bosyi@travelline.ru>
2022-03-06 18:03:34 +0300

    Добавлено задание html_decode

--- lw2/html_decode/html_decode.cpp
+++ lw2/html_decode/html_decode.cpp
@@ -5,18 +5,70 @@
 
 using namespace std;
 
-string replace(string const& source_string, string const& find_string, string const& replace_string)
+void move_window(string& window)
 {
-	string replaced_string = source_string;
-	size_t find_pos = replaced_string.find(find_string);
-
-	while (find_pos != string::npos)
+	for (int i = 1; i < window.size(); i++)
 	{
-		replaced_string.replace(find_pos, find_string.length(), replace_string);
-		find_pos = replaced_string.find(find_string);
+		window[i - 1] = window[i];
+	}
+}
+
+bool is_window_string_possible(string const& window, string const& str)
+{
+	bool is_possible = false;
+
+	int i = 0;
+	while (i < str.size() && !is_possible)
+	{
+		is_possible = window.substr(window.size() - 1 - i, i + 1) == str.substr(0, i + 1) ? true : is_possible;
+		i++;
 	}
 
-	return replaced_string;
+	return is_possible;
+}
+
+string replace(string& source_string, string const& find_string, string const& replace_string)
+{
+	string out_string = "";
+	string window = "";
+
+	for (int i = 0; i < find_string.size(); i++)
+	{
+		window += ' ';
+	}
+
+	string reserve = "";
+
+	if (window.size() != 0)
+	{
+		for (string::iterator source_it = source_string.begin(); source_it != source_string.end(); ++source_it)
+		{
+			move_window(window);
+			window[window.size() - 1] = *source_it;
+
+			bool is_find_string_possible = is_window_string_possible(window, find_string);
+
+			reserve += *source_it;
+
+			if (window == find_string)
+			{
+				out_string += replace_string;
+				reserve = "";
+			}
+
+			if (!is_find_string_possible)
+			{
+				out_string += reserve;
+				reserve = "";
+			}
+		}
+	}
+	else
+	{
+		out_string = source_string;
+	}
+
+	return out_string;
 }
 
 string html_decode(string const& html)
@@ -30,25 +82,47 @@ string html_decode(string const& html)
 	};
 	string decoded_string = html;
 
-	for (pair<string, string> decoder : decoders)
+	for (const auto& [find_string, replace_string] : decoders)
 	{
-		string find_string = decoder.first;
-		string replace_string = decoder.second;
-
 		decoded_string = replace(decoded_string, find_string, replace_string);
 	}
 
 	return decoded_string;
 }
 
-int main()
+void decode(istream& input_stream, ofstream& decoded_lines)
 {
 	string input_line = "";
 
-	while (getline(cin, input_line))
+	while (getline(input_stream, input_line))
 	{
-		cout << html_decode(input_line) << endl;
+		decoded_lines << html_decode(input_line) << endl;
 	}
+}
+
+void print(ifstream& out_lines, ostream& out)
+{
+	string line = "";
+
+	while (getline(out_lines, line))
+	{
+		out << line << endl;
+	}
+}
+
+int main()
+{
+	char* temp_file = tmpnam(NULL);
+
+	ofstream decoded_lines(temp_file);
+	decode(cin, decoded_lines);
+	decoded_lines.close();
+
+	ifstream out_lines(temp_file);
+	print(out_lines, cout);
+	out_lines.close();
+
+	remove(temp_file);
 
 	return 0;
 }
